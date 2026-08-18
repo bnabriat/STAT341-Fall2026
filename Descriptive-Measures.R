002890222c73aec3fa5ec98fdf2d9b485fd608d4
Briana Nabriat <briananabriat@gmail.com>
2026-08-18 16:13:04 -0400

    Add files via upload

---
+++ Descriptive-Measures.R
@@ -0,0 +1,15 @@
+#Name: Briana Nabriat, Data: 08/18/2026, Purpose: to test functions of descriptive stats
+
+#create a variable called variable1 and store 5 numbers from 1-5
+
+variable1 <- c(1,2,3,4,5)
+
+#calculate mean of variable1
+
+mean(variable1)
+[1] 3
+
+#calculate standard deviavtion
+sd(variable1)
+[1] 1.581139
+
